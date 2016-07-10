cab9cbdef8a68d1af08dd1db833d7d33af8b3e37
christineprz <christineperez33@yahoo.com>
2016-07-09 17:25:33 -0700

    edited context inside notes/view

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -7,7 +7,7 @@ class Index(View):
     template_name = 'notes/index.html'
 
     def get_context_data(self):
-        context = {}
+        context = {'notes': Note.objects.order_by('-id'),}
         return context
 
     def get(self, request, *args, **kwargs):
